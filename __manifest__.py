246ed9b0701e40ec5beec66dcdf6a1beff858ff3
katherineES98 <katsantos1997@gmail.com>
2021-10-16 16:02:27 -0600

    implementacion-wizard

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
 
     # always loaded
     'data': [
+         
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import course
 from . import partner
+from . import wizard
